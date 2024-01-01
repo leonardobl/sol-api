0f8bd7b35c2572fe544303271888b21525a3781c
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-01-01 13:03:37 -0300

    remocao de comentarios

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,6 @@ import { AppDataSource } from './data-source';
 
 const port = 3333;
 
-// AppDataSource.initialize().then(() => {
-//   app.listen(port, () => console.log(`Server on port:${port}`));
-// });
-
 const url = 'https://opcoes.net.br/opcoes/bovespa';
 
 (async () => {
@@ -32,10 +28,6 @@ const url = 'https://opcoes.net.br/opcoes/bovespa';
     'button[class="dt-button buttons-excel buttons-html5"]',
   );
   await page.click('button[class="dt-button buttons-excel buttons-html5"]');
-
-  // setTimeout(() => {
-  //   browser.close();
-  // }, 2000);
 })();
 
 app.listen(port, () => console.log(`Server on port:${port}`));
